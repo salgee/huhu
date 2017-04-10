1f7f4307d9de763816aac0a1bfde0dc652c2f9d7
Vongolatt <1085280247@qq.com>
2017-04-10 18:14:27 +0800

    完成主页， 底部导航组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="huhu">
     <router-view></router-view>
   </div>
 </template>
@@ -11,12 +11,12 @@ export default {
 </script>
 
 <style>
-#app {
+body{
+  margin: 0;
+}
+#huhu {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,18 @@
  */
 // 导入我们要跳转的膜版页面
 import homePage from './components/home_page.vue'
-
+import addHouse from './components/add_house.vue'
 // 定义路由集合
 const routes = [
   {
     name: 'homePage',
     path: '/',
     component: homePage
+  },
+  {
+    name: 'add',
+    path: '/addhouse',
+    component: addHouse
   }
 ]
 export default routes
