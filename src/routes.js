b4e6960b6bfda4a643d459a2bc078d0ab3eeb224
yangyixiong <13720534329@qq.com>
2017-04-10 18:33:56 +0800

    更新bug

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,8 @@
  */
 // 导入我们要跳转的膜版页面
 import homePage from './components/home_page.vue'
-<<<<<<< HEAD
 import login from './components/login.vue'
-
-=======
 import addHouse from './components/add_house.vue'
->>>>>>> 1f7f4307d9de763816aac0a1bfde0dc652c2f9d7
 // 定义路由集合
 const routes = [
   {
@@ -17,15 +13,14 @@ const routes = [
     component: homePage
   },
   {
-<<<<<<< HEAD
     name: 'login',
     path: '/login',
     component: login
-=======
+  },
+  {
     name: 'add',
     path: '/addhouse',
     component: addHouse
->>>>>>> 1f7f4307d9de763816aac0a1bfde0dc652c2f9d7
   }
 ]
 export default routes
