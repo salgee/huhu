38c9ecb9b002777be7814ded6a9bce02772cf8d9
yangyixiong <13720534329@qq.com>
2017-04-11 18:16:47 +0800

    我的写了大概

--- src/components/home/user.vue
+++ src/components/home/user.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <router-link tag="span" to="/login" class="wangjipws"><img src="/assets/images/login-img-top.png"></router-link>
+    <mt-cell title="我的评价" is-link></mt-cell>
+    <mt-cell title="优惠券" is-link></mt-cell>
+    <mt-cell title="分享" is-link></mt-cell>
+    <mt-cell title="设置余额支付密码" is-link></mt-cell>
+    <mt-cell title="设置" is-link></mt-cell>
   </div>
 </template>
 
