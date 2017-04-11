7c9e365efd3dce1f87dd9b481cf1ef4a2415a3cd
Vongolatt <1085280247@qq.com>
2017-04-11 16:31:58 +0800

    完善主页内容

---
+++ src/components/home/user.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'addHouse',
+    mounted () {
+
+    },
+    data () {
+      return {}
+    },
+    methods: {},
+    components: {}
+  }
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/home/wallet.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+    export default {
+      name: 'addHouse',
+      mounted () {
+
+      },
+      data () {
+        return {}
+      },
+      methods: {},
+components: {}
+    }
+</script>
+
+<style>
+
+
+</style>
