c40e66e46073a53d65ed5cac4546fbc97b37ac9e
yangyixiong <13720534329@qq.com>
2017-04-11 18:43:59 +0800

    我的

--- src/components/home/user.vue
+++ src/components/home/user.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <router-link tag="span" to="/login" class="wangjipws"><img src="/assets/images/login-img-top.png"></router-link>
+  <div class="user">
+    <div class="user-neirong">
+      <img src="../../assets/images/我的图片.png" alt="" class="user-neirong-beijing">
+      <router-link tag="span" to="/login" class="wangjipws"><img src="../../assets/images/图层-2.png" class="user-neirong-geren"></router-link>
+    </div>
     <mt-cell title="我的评价" is-link></mt-cell>
     <mt-cell title="优惠券" is-link></mt-cell>
     <mt-cell title="分享" is-link></mt-cell>
@@ -24,6 +27,21 @@
 </script>
 
 <style>
-
+.user-neirong{
+  width:100%;
+  height:200px;
+  position: relative;
+}
+.user-neirong-beijing{
+  width:100%;
+  height:100%;
+}
+.user-neirong-geren{
+  width:20%;
+  height:30%;
+  position: absolute;
+  top:30%;
+  left:40%;
+}
 
 </style>
