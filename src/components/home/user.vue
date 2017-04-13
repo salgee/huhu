04fb477579eb99a9308688a8ba864b2fbbb79407
yangyixiong <13720534329@qq.com>
2017-04-13 16:42:09 +0800

    无

--- src/components/home/user.vue
+++ src/components/home/user.vue
@@ -3,6 +3,7 @@
     <div class="user-neirong">
       <img src="../../assets/images/我的图片.png" alt="" class="user-neirong-beijing">
       <div>
+        <mt-actionsheet :actions="actions" v-model="img" ></mt-actionsheet>
         <img src="../../assets/images/图层-2.png"  class="user-neirong-geren" >
         <router-link tag="span" to="/user/login/#" class="wangjipws-anniu">未登录</router-link>
       </div>
