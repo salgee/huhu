fd5d71002bd0d71dbe6cd07a2cfbb39dacba598b
Vongolatt <1085280247@qq.com>
2017-04-20 11:38:56 +0800

    添加微信支付失败，等待ing

--- src/components/join_vip_takeMoney.vue
+++ src/components/join_vip_takeMoney.vue
@@ -18,9 +18,11 @@
     </div>
   </div>
 </template>
+
 <script>
   import {Cell, Radio, Button, Toast} from 'mint-ui'
   import Axios from 'axios'
+//  import wx from 'weixin-js-sdk'
   Axios.defaults.baseURL = 'http://a.com'
   Axios.defaults.headers = {
     'Content-Type': 'application/json',
@@ -87,11 +89,21 @@
       payFor () {
         let vm = this
         if (vm.pay === 'we') {
-          Axios.post('/api/pay/weixin/houseForegif',
+          Axios.post('/api/pay/weixin/house-foregift',
             {
               price: sessionStorage.huhu_bedNum,
               houseId: sessionStorage.huhu_key,
               payType: 'payHouseForegift'
+            }).then((data) => {
+              if (data.data.message === 'isOk') {
+                vm.weixin(data.data.data)
+              } else {
+                Toast({
+                  message: '请求错误，请稍后重试',
+                  position: 'bottom',
+                  duration: 1500
+                })
+              }
             })
         } else {
           Toast({
@@ -101,6 +113,46 @@
           })
         }
       }
+//      weixin (dt) {
+//        console.log(sha1)
+//        Axios.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx612009b09a380c9b&secret=APPSECRET')
+//          .then(function (data) {
+//            console.log(data)
+//          })
+//        let string = `noncestr=${dt.noncestr}&timestamp=${dt.timestamp}&url=${window.encodeURIComponent(location.href.split('#')[0])}`
+//        wx.config({
+//          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+//          appId: dt.appid, // 必填，公众号的唯一标识
+//          timestamp: dt.timestamp, // 必填，生成签名的时间戳
+//          nonceStr: dt.noncestr, // 必填，生成签名的随机串
+//          signature: dt.sign, // 必填，签名，见附录1
+//          jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+//        })
+//        wx.ready(function () {
+//          console.log(12)
+//          wx.chooseWXPay({
+//            timestamp: dt.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+//            nonceStr: dt.noncestr, // 支付签名随机串，不长于 32 位
+//            package: 'prepay_id=' + dt.prepay_id, // prepay_id，提交格式如：prepay_id=***）
+//            signType: 'SHA1', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+//            paySign: dt.sign, // 支付签名
+//            success: function (res) {`  `
+//              // 支付成功后的回调函数
+//              console.log(res, 1)
+//            }
+//          })
+//          wx.error(function (res) {
+//            console.log(res)
+//          })
+//        })
+//        wx.checkJsApi({
+//          jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+//          success: function (res) {
+//            // 以键值对的形式返回，可用的api值true，不可用为false
+//            // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+//          }
+//        })
+//      }
     },
     components: {
       Cell,
